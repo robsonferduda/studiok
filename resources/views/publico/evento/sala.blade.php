9c4a575c64dfed3dcd7ef04e4bdd3585271fcac9
Robson Fernando Duda <robsonferduda@gmail.com>
2021-11-03 18:27:11 -0300

    Alteração Template

--- resources/views/publico/evento/sala.blade.php
+++ resources/views/publico/evento/sala.blade.php
@@ -9,7 +9,7 @@
                 <div class="col-lg-12 col-md-12 align-self-center">  
                     <h4 class="mb-1 mt-2"><strong> {{ $atividade->sala->nm_sala_sal }}</h4></strong> 
                     <h4 class="mb-1 mt-2"><strong></strong> {{ $atividade->nm_atividade_ati }}</h4>  
-                    <div class="text-right">
+                    <div>
                         Atividade iniciada em {{ Carbon\Carbon::parse($atividade->dt_inicio_atividade_ati)->format('d/m/Y H:i') }} e término em {{ Carbon\Carbon::parse($atividade->dt_termino_atividade_ati)->format('d/m/Y H:i') }} 
                         <span class="badge badge-success">ONLINE</span>      
                     </div>       
