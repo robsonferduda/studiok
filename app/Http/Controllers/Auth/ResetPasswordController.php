32e22d2ed1d9bca58457aebb9a865dda9dbc60a8
Robson Fernando Duda <robsonferduda@gmail.com>
2021-11-04 08:25:07 -0300

    Recuperação de Senha

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -32,6 +32,8 @@ class ResetPasswordController extends Controller
 
     public function showResetForm(Request $request, $token = null)
     {
+        dd($request->email);
+
         return view('auth.passwords.reset')->with(
             ['token' => $token, 'email' => $request->email]
         );
