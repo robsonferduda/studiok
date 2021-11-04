44cad96a4ec93a1c56600bb4f35131f1e6a8408c
Robson Fernando Duda <robsonferduda@gmail.com>
2021-11-03 22:27:06 -0300

    Alterações Interface

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -51,9 +51,6 @@ class EventoController extends Controller
     {
         $evento = Evento::find($id);
         $participante = Participante::where('id_pessoa_pes', Auth::user()->id_pessoa_pes)->first();
-
-        dd($participante);
-
         $participante->eventos()->attach($evento);
 
         return redirect('dashboard')->withInput();
