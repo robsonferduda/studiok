760c267c84195419990c4dd0db58ce55052471d2
Robson Fernando Duda <robsonferduda@gmail.com>
2021-11-04 08:18:27 -0300

    Recuperação de Senha

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -27,7 +27,7 @@ class ResetPassword extends Notification
             ->subject('Recuperação de Senha (Não Responda)')
             ->markdown('auth.reset.email')
             ->line('Você está recebendo este e-mail porque recebemos uma solicitação de redefinição de senha para sua conta.')
-            ->action(Lang::get('Atualizar Senha'), url(config('app.url').route('password.reset', ['token' => $this->token, 'email' => urlencode($notifiable->email)], false)))
+            ->action(Lang::getFromJson('Atualizar Senha'), url(config('app.url').route('password.reset', ['token' => $this->token, 'email' => urlencode($notifiable->email)], false)))
             ->line(Lang::get('Este link de redefinição de senha irá expirar em :count minutos.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
             ->line('Se você não solicitou a redefinição de senha, nenhuma ação adicional será necessária.');
     }
