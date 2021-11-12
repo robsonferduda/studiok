a51b62acecdf1833a4ce3dc030659670ed6f2fa9
Robson Fernando Duda <robsonferduda@gmail.com>
2021-11-12 16:25:54 -0300

    Listagem de Eventos

--- resources/views/seminario.blade.php
+++ resources/views/seminario.blade.php
@@ -6,7 +6,7 @@
     <div class="col-lg-8 col-md-8 m-auto">
         <div class="row">
             <div class="col-lg-6 col-md-6 ml-auto mb-4">
-                <img src="{{ url('img/workshop-egc.jpeg') }}">
+                <img src="{{ url('img/eventos/ciki_2021.jpeg') }}">
             </div>
             <div class="col-lg-6 col-md-6 ml-auto box-programacao">
                 <p class="menu-evento"><i class="fa fa-calendar ml-2"></i><a href="{{ url('eventos/'.Session::get('evento')->ds_apelido_eve.'/programacao') }}"> Programação</a></p>
