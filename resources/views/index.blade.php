ab6adda55c49448def7b215ce0d0116b64ad5f5e
Robson Fernando Duda <robsonferduda@gmail.com>
2021-11-18 12:26:09 -0300

    Acesso

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.guest')
 @section('content')
   <div class="row">
-    <div class="col-lg- col-md-6 mr-auto">
+    <div class="col-lg-6 col-md-6 mr-auto">
         <div class="card card-signup text-center">
           <div class="card-body">
             <a href="{{ url('eventos/ciki-2021') }}"><img src="img/eventos/ciki_2021.jpeg"></a>
--- resources/views/seminario.blade.php
+++ resources/views/seminario.blade.php
@@ -15,6 +15,12 @@
                 <p class="menu-evento"><i class="fa fa-video ml-2"></i><a href="{{ url('eventos/'.Session::get('evento')->ds_apelido_eve.'/zoom') }}"> Endereços Zoom</a></p>
                 <p class="menu-evento"><i class="fa fa-group ml-2"></i><a href="{{ url('eventos/'.Session::get('evento')->ds_apelido_eve.'/conferencistas') }}"> Conferencistas</a></p>
                 <p class="menu-evento"><i class="fa fa-envelope ml-2"></i><a href="{{ url('eventos/'.Session::get('evento')->ds_apelido_eve.'/contato') }}"> Contato</a></p>
+
+                <div class="col-lg-12 col-md-12 ml-auto mb-4 pb-3 bg-white rd-12 text-center">
+                    <h5 class="mt-3 mb-3 pt-3">Acesso ao Público</h5>
+                    <p>Todo o conteúdo do Ciki pode ser assitido de forma aberta clicando no endereço abaixo.</p>
+                    <a href="https://www.youtube.com/playlist?list=PLlrwBGCrNusnJJhVTxRFaNnK2roiL8kc7" target="_BLANK"><i class="nc-icon nc-tv-2"></i> Clique aqui</a>
+                </div>
             </div>
         </div>
     </div>
